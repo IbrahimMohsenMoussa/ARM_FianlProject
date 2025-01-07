07924bd24aae50cda004472839ebbfe6b2d39eff
IbrahimMohsenMoussa <ibrahim.moussa04@eng-st.cu.edu.eg>
2025-01-07 09:14:14 +0200

    Update Files

--- Systick_CCS_WS/Systick/MCAL/Systick/Systick_std_types.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef MCAL_SYSTICK_SYSTICK_STD_TYPES_H_
-#define MCAL_SYSTICK_SYSTICK_STD_TYPES_H_
-#include"std_types.h"
-
-typedef  uint16   SYSTICK_TIMEMS;
-#define SYSTICK_ZERO (0u)
-
-#endif /* MCAL_SYSTICK_SYSTICK_STD_TYPES_H_ */
